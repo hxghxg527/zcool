06da4a0aff108607de11ce32740385114ed19b31
hxghxg527 <hxghxg527@163.com>
2016-03-12 17:02:32 +0800

    add carousel item and set their css.

--- js/carousel.js
+++ js/carousel.js
@@ -5,18 +5,34 @@
 
 function Carousel(options) {
     this.options = $.extend({}, this.options || {}, options);
+    this.currentItemIndex = 0;
     this.indicatorContainer = $('.indicator-container');
     this.indicators = this.indicatorContainer.find('.indicator-item');
+    this.carouselItemInnerContainer = $('.carousel-item-inner-container');
+    this.carouselItems = this.carouselItemInnerContainer.find('.carousel-item');
+    this.itemsNum = this.carouselItems.length;
+
     this.init();
 }
 
 Carousel.prototype = {
     init: function () {
-        this.setPosition();
+        this.setIndicatorPosition();
+        this.configCarouselStyles();
     },
-    setPosition: function () {
+    setIndicatorPosition: function () {
         this.indicatorContainer.css({
             marginLeft: -(this.indicatorContainer.outerWidth() / 2),
+            display: 'block'
+        });
+    },
+    configCarouselStyles: function () {
+        this.carouselItems.css({
+            width: 100 / this.itemsNum + '%'
+        });
+        this.carouselItemInnerContainer.css({
+            width: 100 * this.itemsNum + '%',
+            left: '0%',
             display: 'block'
         });
     }
